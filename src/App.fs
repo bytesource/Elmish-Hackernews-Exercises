bf6795f0077f2d7cf87ee0e12b8bae58a91e9be2
bytesource <stefan.rohlfing@gmail.com>
2020-05-14 12:04:54 +0800

    Implement types and init()

--- src/App.fs
+++ src/App.fs
@@ -6,6 +6,37 @@ open Elmish
 open Elmish.React
 open Feliz
 
+type ValueStatus<'t> =
+    | NotStarted
+    | InProgress
+    | Resolved of 't
+
+
+type AsyncStatus<'t> =
+    | Started 
+    | Finished of 't
+
+
+type HackerNewsItem = {
+    Id: int
+    Title: string
+    Url: string option
+}
+
+
+type Msg =
+    | LoadStoryItems of AsyncStatus<Result<HackerNewsItem list, string>>
+
+
+type State = {
+    StoryItems: ValueStatus<Result<HackerNewsItem list, string>>
+}
+
+let init() =
+    { StoryItems = NotStarted }, Cmd.ofMsg (LoadStoryItems Started)
+
+
+
 
 Program.mkProgram init update render
 |> Program.withReactSynchronous "elmish-app"
